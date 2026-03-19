a3c8ff82cfe8fb18a7b83f52212ee3bc7b5ea1b3
Abu Bakar Siddik <siddik@askturing.ai>
2026-03-19 14:50:08 +0600

    build(deps): add pypi publishing tools and update version
    Add `build` and `twine` to development dependencies to facilitate package
    distribution. Update internal version strings and remove the
    v0.1.0 next steps planning document.

--- src/pagans/__version__.py
+++ src/pagans/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev17+g6d5b0bd25.d20250921'
-__version_tuple__ = version_tuple = (0, 1, 'dev17', 'g6d5b0bd25.d20250921')
+__version__ = version = '0.1.dev25+g21b5daeb1.d20260319'
+__version_tuple__ = version_tuple = (0, 1, 'dev25', 'g21b5daeb1.d20260319')
 
-__commit_id__ = commit_id = 'g6d5b0bd25'
+__commit_id__ = commit_id = 'g21b5daeb1'
